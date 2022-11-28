933c5ad1248c3193e7913dbc5fb4c72fa9d47ce2
Papittson <cynthia.delgard@gmail.com>
2022-11-28 14:12:30 +0100

    update UML

---
+++ app/src/main/java/com/example/jobretriever/models/User.java
@@ -0,0 +1,11 @@
+package com.example.jobretriever.models;
+
+public class User {
+    String firstname;
+    String mail;
+    String name;
+    String nationality;
+    String password;
+    String phone;
+    String type;
+}

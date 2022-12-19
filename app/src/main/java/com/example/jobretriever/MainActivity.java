15ca54a339f2c01df53c69391eb8c6eb64bce4b8
mayosine <farasyaninda@gmail.com>
2022-12-19 15:26:13 +0100

    welcome, sign in, sign up page (+fragments)

--- app/src/main/java/com/example/jobretriever/MainActivity.java
+++ app/src/main/java/com/example/jobretriever/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.jobretriever;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -18,16 +24,43 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "pouet";
     FirebaseFirestore db;
+    Button signIn, signUp;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_JobRetriever);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = FirebaseFirestore.getInstance();
         Map<String, Object> user = new HashMap<>();
 
+        signIn = findViewById(R.id.SignInBtn);
+        signUp = findViewById(R.id.SignUpBtn);
+        back = findViewById(R.id.LogoWelcome);
 
+        replaceFragment(new fragment_welcome());
 
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new fragment_sign_in());
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new fragment_sign_up());
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new fragment_welcome());
+            }
+        });
 
 // Add a new document with a generated ID
         db.collection("users")
@@ -44,6 +77,13 @@ public class MainActivity extends AppCompatActivity {
                         Log.w(TAG, "Error adding document", e);
                     }
                 });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fM = getSupportFragmentManager();
+        FragmentTransaction fT = fM.beginTransaction();
+        fT.replace(R.id.welcomePage, fragment);
+        fT.commit();
     }
 
 }
---
+++ app/src/main/java/com/example/jobretriever/fragment_sign_in.java
@@ -0,0 +1,67 @@
+package com.example.jobretriever;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fragment_sign_in#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragment_sign_in extends Fragment {
+
+    View view;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_EMAIL = "email";
+    private static final String ARG_PWD = "password";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public fragment_sign_in() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment fragment_sign_in.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static fragment_sign_in newInstance(String param1, String param2) {
+        fragment_sign_in fragment = new fragment_sign_in();
+        Bundle args = new Bundle(2);
+        args.putString(ARG_EMAIL, param1);
+        args.putString(ARG_PWD, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_EMAIL);
+            mParam2 = getArguments().getString(ARG_PWD);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        return view;
+    }
+}

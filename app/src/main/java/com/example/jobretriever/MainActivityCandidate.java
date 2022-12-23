98a54cb406f16dd114a4bef9b3fa9898231d35a3
mayosine <farasyaninda@gmail.com>
2022-12-23 12:17:23 +0100

    navbar

--- app/src/main/java/com/example/jobretriever/MainActivityCandidate.java
+++ app/src/main/java/com/example/jobretriever/MainActivityCandidate.java
@@ -6,8 +6,13 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivityCandidate extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,11 @@ public class MainActivityCandidate extends AppCompatActivity {
         setContentView(R.layout.activity_main_candidate);
 
         replaceFragment(new candidate_home());
+
+        BottomNavigationView navbar = findViewById(R.id.navbar);
+
+
+
     }
 
     private void replaceFragment(Fragment fragment) {
